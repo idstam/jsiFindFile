4359e8e5e9facc9f24b8b09aab11bd011a540232
Johan Idstam <johan@idstam.com>
2016-04-18 17:49:51 +0200

    ..

---
+++ jsiFindFile_net/Search.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jsiGrepWinForm
+{
+    public class SearchManager
+    {
+        private string _rootFolder;
+        private List<string> _originalFiles;
+        private List<Match> _result;
+        private bool _stop;
+        private string[] _includeFilters;
+        private string[] _excludeFilters;
+        private string _needle;
+        public SearchManager(string[] includeFilters, string[] excludeFilters, string needle)
+        {
+            _includeFilters = includeFilters?.Length > 0 ? includeFilters : null;
+            _excludeFilters = excludeFilters?.Length > 0 ? excludeFilters : null;
+            _needle = needle;
+        }
+
+        public List<Match> Search(string rootFolder, List<Match> matches = null)
+        {
+            if (matches == null) matches = new List<Match>();
+            _rootFolder = rootFolder;
+            var folderContent = GetFolderContent(rootFolder);
+
+			matches.AddRange(SearchFiles(folderContent.Item1.ToList()));
+
+            //foreach (var folder in folderContent.Item2)
+            Parallel.ForEach(folderContent.Item2, folder =>
+            {
+                Search(folder, matches);
+            });
+
+            return matches;
+        }
+
+        public List<Match> Search(List<string> files)
+        {
+            _originalFiles = files;
+            return SearchFiles(files);
+        }
+
+        private List<Match> SearchFiles(List<string> files)
+        {
+            var ret = new List<Match>();
+
+            //foreach (var f in files)
+            Parallel.ForEach(files, f =>
+            {
+                if (!IncludeFile(f)) return;
+                if (ExcludePathContaining(f)) return;
+
+                var match = SearchFile(f);
+                if (match != null) ret.Add(match);
+            });
+
+            return ret;
+        }
+
+        private Tuple<string[], string[]> GetFolderContent(string rootFolder)
+        {
+            var files = Directory.GetFiles(rootFolder);
+            var folders = Directory.GetDirectories(rootFolder);
+            return new Tuple<string[], string[]>(files, folders);
+        }
+
+        private bool IncludeFile(string filename)
+        {
+            if (_includeFilters== null) return true;
+
+            foreach (var filter in _includeFilters)
+            {
+                if (filename.ToUpperInvariant().Like(filter)) return true;
+            }
+            return false;
+        }
+
+        private bool ExcludePathContaining(string filename)
+        {
+            if (_excludeFilters == null) return false;
+
+            foreach (var filter in _excludeFilters)
+            {
+                if (filename.ToUpperInvariant().Contains(filter))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private Match SearchFile(string fileName)
+        {
+            var needle = _needle.ToUpperInvariant();
+            var match = new Match(fileName, _needle);
+
+            var lineNumber = 0;
+            foreach (var line in File.ReadLines(fileName))
+            {
+                lineNumber++;
+                var upperLine = line.ToUpperInvariant();
+                if (upperLine.Contains(needle))
+                {
+                    match.MatchLines.Add(new MatchLine()
+                    {
+                    	Line = line,
+                        LineNumber = lineNumber,
+                        Position = upperLine.IndexOf(needle, StringComparison.InvariantCultureIgnoreCase)
+                    });
+
+                    OnFoundMatch(new MatchEventArgs {Match =  match});
+                }
+            }
+
+            return match.MatchLines.Count > 0 ? match : null;
+        }
+
+        public event EventHandler FoundMatch ;
+        private void OnFoundMatch(MatchEventArgs e)
+        {
+            var handler = FoundMatch;
+            handler?.Invoke(this, e);
+        }
+    }
+}
